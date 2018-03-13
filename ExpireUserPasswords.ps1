a69973c9f98dcc8406f065f30c462738c45f3730
Joel Perez-Sanchez <jperezsanchez@opentable.com>
2018-03-13 14:22:00 -0700

    Removed an unessary script, also minor mods to some scripts

--- ExpireUserPasswords.ps1
+++ ExpireUserPasswords.ps1
@@ -1,4 +1,4 @@
-﻿# Script to disable accounts from list
+﻿# Script to expire AD passwords from a list
 # Account name must be in from of SamAccountName
 
 $file = Read-Host 'What is the path to the file'
